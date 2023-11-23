175be7f45c37e1722b5d974510a84d2f1696feff
bw007 <abdulaziz00791@gmail.com>
2023-11-23 07:31:27 +0500

    Registration success

---
+++ js/lib/login.js
@@ -0,0 +1,10 @@
+const form = document.querySelector(".form__holder");
+
+if (form) {
+}
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  
+  console.log(10);
+}
---
+++ js/lib/reg.js
@@ -0,0 +1,34 @@
+const form = document.querySelector(".form__holder");
+
+if (form) {
+}
+
+const reg = (e) => {
+  e.preventDefault();
+  
+  let user = {};
+
+  let data = new FormData(form);
+
+  data.forEach((value, name) => {
+    user[name] = value;
+  });
+
+  fetch(`${URL}/auth/reg`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+  .then(res => res.text())
+  .then(data => {
+    console.log(data);
+  })
+  .finally(log => {
+    console.log("Final", log);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+};
